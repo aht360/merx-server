9288afdbc8f26dcd2f5a74c9738acbb03e7d63be
Jhonatan <jhonatankennedy@live.com>
2020-05-04 17:24:19 -0300

    calc verde

--- src/pages/Simulação/index.js
+++ src/pages/Simulação/index.js
@@ -1,95 +1,248 @@
-import React,{useState,useEffect} from 'react'
-import NavBar from '../../Components/NavBar/navBar.js'
-import {InputContainer} from './styled.js'
-import api from '../../Services/api.js'
-import Simulator from './simulator.js'
+import React, { useState, useEffect } from 'react';
+import NavBar from '../../Components/NavBar/navBar.js';
+import { InputContainer } from './styled.js';
+import api from '../../Services/api.js';
+import Simulator from './simulator.js';
 
+export default function Simulacao() {
+  const [cliente, setCliente] = useState('');
+  const [cnpj, setCnpj] = useState();
+  const [distribuidora, setDistribuidora] = useState('CELPE');
+  const [ths, setThs] = useState('VERDE');
+  const [grupoTarifa, setGrupoTarifa] = useState('A4');
+  const [demanda, setDemanda] = useState();
+  const [demandaFpu, setDemandaFpu] = useState();
+  const [consumoP, setConsumoP] = useState();
+  const [consumoPf, setConsumoPf] = useState();
 
-export default function Simulacao(){
+  const [demandaP,setDemandaP] = useState();
+  const [demandaPu,setDemandaPu] = useState()
 
-    const [demanda,setDemanda] = useState(0)
-    const [demandaUlt,setDemandaUlt] =useState(0)
-    const [consumoPonta,setConsumoPonta] = useState(0)
-    const [consumoPontaFora,setConsumoPontaFora] = useState(0)
-    const [usaGerador,setUsaGerador] = useState('sim')
-    const [consumoGerador,setConsumoGerador] = useState(0)
-    const [custoGeracaoDisel,setCustoGeracaoDisel] = useState(0)
-    const [distribuidora,setDistribuidora] = useState('celpe')
-    const [grupoTarifa,setGrupoTarifa] = useState('a4')
-    const [icms,setIcms] = useState(0)
-    const [pis,setPis] = useState(0)
-    const [cofin,setCofins] = useState(0)
-    const [fonte,setFonte] = useState('conv')
-    const [inputs,setInputs] = useState({})
-    const [results,setResults] = useState({})
+  const [usaGerador, setUsaGerador] = useState('sim');
+  const [geradorACL,setgeradorACL] = useState ('sim')
+  const [consumoGerador, setConsumoGerador] = useState('N/A');
+  const [custoGeracaoDisel, setCustoGeracaoDisel] = useState();
 
-    async function handleSimulation(e){
-        e.preventDefault()
-        const planilha = {demanda,demandaUlt,consumoPonta,consumoPontaFora,usaGerador,consumoGerador,custoGeracaoDisel,distribuidora,grupoTarifa,icms,pis,cofin,fonte}    
+  const [calc, setCalc] = useState('CONV');
+  const [desconto,setDesconto] = useState('0.25')
+  const [icms, setIcms] = useState();
+  const [pis, setPis] = useState();
+  const [cofin, setCofins] = useState();
+  const [inputs, setInputs] = useState({});
+  const [resultCativo, setResultCativo] = useState({});
+  const [resultLivre, setResultLivre] = useState({});
 
-        try{
-           const values = await api.post('simulacao',planilha)
-           const {input,resultCativo} = values.data
-           setInputs(input)
-           setResults(resultCativo)
-        }catch(err){
-            alert('Erro, tente novamente.')
-        }
+  async function handleSimulation(e) {
+    e.preventDefault();
+    const planilha = {
+      cliente,
+      cnpj,
+      distribuidora,
+      ths,
+      grupoTarifa,
+      demanda,  
+      demandaFpu,
+      consumoP,
+      consumoPf,
+      demandaP,
+      demandaPu,
+      usaGerador,
+      geradorACL,
+      consumoGerador,
+      custoGeracaoDisel,
+      calc,
+      desconto,
+      icms,
+      pis,
+      cofin,
+    };
 
+    try {
+      const results = await api.post('simulacao', planilha);
+      const { input,ResultCativo,ResultLivre} = results.data;
+      setInputs(input);
+      setResultCativo(ResultCativo);
+      setResultLivre(ResultLivre)
+    } catch (err) {
+      alert('Erro, tente novamente.');
     }
-    return(
-        <div>
-            <NavBar/>
-            <InputContainer>
-                <form onSubmit={handleSimulation}>
-                    <span>Demanda:<input type="text" value={demanda} onChange={e=>setDemanda(e.target.value)}/> kW </span>
-                    <span>Demanda Ultrapassagem:<input type="text" value={demandaUlt} onChange={e=>setDemandaUlt(e.target.value)}/>kW</span>
-                    <span>Consumo de Ponta:<input type="text" value={consumoPonta} onChange={e=>setConsumoPonta(e.target.value)}/>kWh</span>
-                    <span>Consumo Fora de Ponta:<input type="text"  value={consumoPontaFora} onChange={e=>setConsumoPontaFora(e.target.value)}/>kWh</span><br></br>
+  }
+  return (
+    <div>
+      <NavBar />
+      <InputContainer>
+        <form onSubmit={handleSimulation}>
+          <span>Cliente:
+            <input
+              type="text"
+              value={cliente}
+              onChange={(e) => setCliente(e.target.value)}/>
+          </span>
+          <span>Cnpj:
+            <input
+              type="text"
+              value={cnpj}
+              onChange={(e) => setCnpj(e.target.value)}/>
+          </span>
 
-                    <span>Usa Gerador:
-                    <select id="usa-gerador" value={usaGerador} onChange={e=>setUsaGerador(e.target.value)}>
-                        <option value="sim">Sim</option>
-                        <option value="nao">Não</option>
-                    </select></span><br></br>
+          <span>Distribuidora:
+            <select
+              id="distribuidora"
+              value={distribuidora}
+              onChange={(e) => setDistribuidora(e.target.value)}>
+              <option value="CELPE">CELPE</option>
+              <option value="CEAL">CEAL</option>
+              <option value="CEMAR">CEMAR</option>
+              <option value="CEPISA">CEPISA</option>
+              <option value="COELBA">COELBA</option>
+              <option value="COSERN">COSERN</option>
+              <option value="ENEL CE">ENEL CE</option>
+              <option value="EPB">EPB</option>
+              <option value="ESE">ESE</option>
+            </select>
+          </span>
 
-                    <span>Consumo Gerador:<input type="text" value={consumoGerador} onChange={e=>setConsumoGerador(e.target.value)}/></span>
-                    <span>Custo da Geração Disel:<input type="text" value={custoGeracaoDisel} onChange={e=>setCustoGeracaoDisel(e.target.value)}/></span><br></br>
+          <span>Ths:
+            <select
+              id="ths"
+              value={ths}
+              onChange={(e) => setThs(e.target.value)}>
+              <option value="VERDE">Verde</option>
+              <option value="AZUL">Azul</option>
+            </select>
+          </span>
+          <span>Grupo Tarifário:
+            <select
+              id="grupo-tarifa"
+              value={grupoTarifa}
+              onChange={(e) => setGrupoTarifa(e.target.value)}>
+              <option value="A4">A4</option>
+              <option value="A3">A3</option>
+            </select>
+          </span>
+          <br></br>
 
-                    <span>Distribuidora:
-                    <select id="distribuidora" value={distribuidora} onChange={e=>setDistribuidora(e.target.value)}>
-                        <option value="celpe">CELPE</option>
-                        <option value="cemar">CEMAR</option>
-                        <option value="cepisa">CEPISA</option>
-                        <option value="cpflPaulista">CPFL PAULISTA</option>
-                        <option value="eletropaulo">ELETROPAULO</option>
-                        <option value="enelCe">ENEL</option>
-                        <option value="epb">EPB</option>
-                        <option value="ese">ESE</option>
-                        <option value="coelba">COELBA</option>
-                    </select></span>
-                    <br></br>
-                    <span>Grupo Tarifário:<select id="grupo-tarifa" value={grupoTarifa} onChange={e=>setGrupoTarifa(e.target.value)}>
-                        <option value="a4">A4</option>
-                        <option value="a3">A3</option>
-                    </select></span>
-                    <br></br>
-                    <span>icms:<input type="text" value={icms} onChange={e=>setIcms(e.target.value)}/>%</span>
-                    <span>pis:<input type="text" value={pis} onChange={e=>setPis(e.target.value)}/>%</span>
-                    <span>cofin:<input type="text" value={cofin} onChange={e=>setCofins(e.target.value)}/>%</span>
-                    <br></br>
-                    <span>Tipo de fonte:<select id="tipo-fonte" value={fonte} onChange={e=>setFonte(e.target.value)}>
-                        <option value="conv">Convencional</option>
-                        <option value="i5">i5</option>
-                        <option value="i1">i1</option>   
-                    </select></span>
-                    <br></br>
-                    <br></br>     
-                    <button>Simular</button>
-                </form>  
-                <br></br><br></br><br></br>
-                <Simulator inputs={inputs} results={results}></Simulator>
-            </InputContainer>
-        </div>
-    )
+          <span>Demanda:
+            <input
+              type="text"
+              value={demanda}
+              onChange={(e) => setDemanda(e.target.value)}/>
+            kW
+          </span>
+          <span>Demanda Fora Ponta Ultrapassagem:
+            <input
+              type="text"
+              value={demandaFpu}
+              onChange={(e) => setDemandaFpu(e.target.value)}/>
+            kW
+          </span>
+
+          <span>Consumo de Ponta:
+            <input
+              type="text"
+              value={consumoP}
+              onChange={(e) => setConsumoP(e.target.value)}/>
+            kWh
+          </span>
+          <span>Consumo Fora de Ponta:
+            <input
+              type="text"
+              value={consumoPf}
+              onChange={(e) => setConsumoPf(e.target.value)}/>
+            kWh
+          </span>
+          <br></br>
+          {(ths==="AZUL") && <span>Demanda Ponta:<input type="text" value={demandaP} onChange={(e) => setDemandaP(e.target.value)}/></span>}
+          {(ths==="AZUL") && <span>Demanda Ponta Ultrapassagem:<input type="text" value={demandaPu} onChange={(e) => setDemandaPu(e.target.value)}/></span>}
+          
+          <span>Usa Gerador:
+            <select
+              id="usa-gerador"
+              value={usaGerador}
+              onChange={(e) => setUsaGerador(e.target.value)}>
+              <option value="sim">Sim</option>
+              <option value="nao">Não</option>
+            </select>
+          </span>
+          <br></br>
+
+          <span>Gerador no ACL:
+            <select
+              id="gerador-acl"
+              value={geradorACL}
+              onChange={(e) => setgeradorACL(e.target.value)}>
+              <option value="sim">Sim</option>
+              <option value="nao">Não</option>
+            </select>
+          </span>
+          <br></br>
+
+          <span>Consumo Gerador:
+            <input
+              type="text"
+              value={consumoGerador}
+              onChange={(e) => setConsumoGerador(e.target.value)}/>
+          </span>
+          <span>Custo da Geração Disel:
+            <input
+              type="text"
+              value={custoGeracaoDisel}
+              onChange={(e) => setCustoGeracaoDisel(e.target.value)}/>
+          </span>
+          <br></br>
+          
+
+          <span>Tipo de calculo:
+            <select
+              id="tipo-calc"
+              value={calc}
+              onChange={(e) => setCalc(e.target.value)}>
+              <option value="ML">Mercado Livre</option>
+              <option value="APE">Autoprodutor</option>
+            </select>
+          </span>
+          <span>DESCONTO:
+            <select
+              id="desconto"
+              value={desconto}
+              onChange={(e) => setDesconto(e.target.value)}>
+              <option value="0.25">25%</option>
+              <option value="0.5">50%</option>
+              <option value="1">100%</option>
+            </select>
+          </span>
+
+          <span>icms:
+            <input
+              type="text"
+              value={icms}
+              onChange={(e) => setIcms(e.target.value)}/>
+            %
+          </span>
+          <span>pis:
+            <input
+              type="text"
+              value={pis}
+              onChange={(e) => setPis(e.target.value)}/>
+            %
+          </span>
+          <span>cofin:
+            <input
+              type="text"
+              value={cofin}
+              onChange={(e) => setCofins(e.target.value)}/>
+            %
+          </span>
+          <br></br>
+          <br></br>
+          <br></br>
+          <button>Simular</button>
+        </form>
+        <br></br>
+        <br></br>
+        <br></br>
+        <Simulator inputs={inputs} resultCat={resultCativo}  resultLiv={resultLivre}></Simulator>
+      </InputContainer>
+    </div>
+  );
 }
--- src/pages/Simulação/simulator.js
+++ src/pages/Simulação/simulator.js
@@ -4,10 +4,24 @@ import {Table} from './styled.js'
 export default function Simulator(props){
 
     const inputs = props.inputs
-    const results = props.results
+    const ResultCativo = props.resultCat
+    const ResultLivre = props.resultLiv
 
-    const {tarifaDemanda,tarifaUltDem,tarifaConsumoPonta,tarifaConsumoForaPonta,totalDemanda,totalUltDem,totalConsumoPonta,totalConsumoForaPonta,totalGerador,total} = results
-    const {demanda,demandaUlt,consumoPonta,consumoPontaFora,usaGerador,consumoGerador,custoGeracaoDisel,distribuidora,grupoTarifa,icms,pis,cofin,fonte} = inputs
+    const {cativoDemandaUnica,cativoDemandaUltrapassagem,cativoConsumoPonta,cativoConsumoPontaFora,qtdGerador,
+        totalDemandaUnica,totalDemandaUltrapassagem,totalConsumoPonta,totalcativoConsumoPontaFora,totalGerador
+        } = ResultCativo
+
+    const {livreDemandaUnica,livreConsumoPonta,livreConsumoPontaFora,livreGerador,energiaACL,
+                    tarifaLivreDemandaUnica,tarifaLivreConsumoPonta,tarifaLivreConsumoForaPonta,
+                    custoGeracaoDisel,energiaACLTarifa,totalLivreDemanda,totalLivreConsumoPonta,
+                    totalLivreConsumoForaPonta,totalLivreGerador,totalEnergiaACL,livreIcms,totalIcms,
+                    totalCativo,totalLivre,economiaLivre,economiaPorcentagem
+            }= ResultLivre 
+
+
+
+    const{cliente, cnpj, distribuidora, ths, grupoTarifa, demanda, demandaFpu, consumoP, consumoPf, demandaP,
+                demandaPu, usaGerador, geradorACL, consumoGerador, desconto,} = inputs
     return(
         <Table>
             <table border="2px">
@@ -15,43 +29,43 @@ export default function Simulator(props){
                     <td>Cativo</td>
                     <td>Qtd</td>
                     <td>Tarifa</td>
-                    <td>$</td>
+                    <td></td>
                 </tr>
                 <tr>
                     <td>Demanda:</td>
                     <td>{demanda}</td>
-                    <td>{tarifaDemanda}</td>
-                    <td>{totalDemanda}</td>
+                    <td>{cativoDemandaUnica}</td>
+                    <td>{totalDemandaUnica}</td>
                 </tr>
                 <tr>
-                    <td>Ultrapassagem Dem:</td>
-                    <td>{demandaUlt}</td>
-                    <td>{tarifaUltDem}</td>
-                    <td>{totalUltDem}</td>
+                    <td>Demanda Ultrapassagem:</td>
+                    <td>{demandaFpu}</td>
+                    <td>{cativoDemandaUltrapassagem}</td>
+                    <td>{totalDemandaUltrapassagem}</td>
                 </tr>
                 <tr>
-                    <td>Consumo Ponta:</td>
-                    <td>{consumoPonta}</td>
-                    <td>{tarifaConsumoPonta}</td>
+                    <td>Consumo ponta:</td>
+                    <td>{consumoP}</td>
+                    <td>{cativoConsumoPonta}</td>
                     <td>{totalConsumoPonta}</td>
                 </tr>
                 <tr>
-                    <td>Consumo Fora Ponta:</td>
-                    <td>{consumoPontaFora}</td>
-                    <td>{tarifaConsumoForaPonta}</td>
-                    <td>{totalConsumoForaPonta}</td>
+                    <td>Consumo ponta Fora:</td>
+                    <td>{consumoPf}</td>
+                    <td>{cativoConsumoPontaFora}</td>
+                    <td>{totalcativoConsumoPontaFora}</td>
                 </tr>
                 <tr>
-                    <td>gerador:</td>
-                    <td>{consumoGerador}</td>
+                    <td>Gerador:</td>
+                    <td>{qtdGerador}</td>
                     <td>{custoGeracaoDisel}</td>
-                    <td>{totalGerador}</td>     
+                    <td>{totalGerador}</td>
                 </tr>
                 <tr>
-                    <td>Total:</td>
-                    <td> xx</td>
-                    <td> xx</td>
-                    <td>{total}</td>
+                    <tr>Total:{totalCativo}</tr>
+                    <tr></tr>
+                    <tr></tr>
+                    <tr></tr>
                 </tr>
             </table>
             <table border="2px">
@@ -63,45 +77,45 @@ export default function Simulator(props){
                 </tr>
                 <tr>
                     <td>Demanda:</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td>{livreDemandaUnica}</td>
+                    <td>{tarifaLivreDemandaUnica}</td>
+                    <td>{totalLivreConsumoPonta}</td>
                 </tr>
                 <tr>
                     <td>Consumo Ponta:</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td>{livreConsumoPonta}</td>
+                    <td>{tarifaLivreConsumoPonta}</td>
+                    <td>{totalLivreConsumoPonta}</td>
                 </tr>
                 <tr>
                     <td>Consumo Fora Ponta:</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>Total:</td>
-                    <td>xx</td>
-                    <td>xx</td>
-                    <td></td>
+                    <td>{livreConsumoPontaFora}</td>
+                    <td>{tarifaLivreConsumoForaPonta}</td>
+                    <td>{totalLivreConsumoForaPonta}</td>
                 </tr>
                 <tr>
                     <td>Gerador:</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td>{livreGerador}</td>
+                    <td>{custoGeracaoDisel}</td>
+                    <td>{totalLivreGerador}</td>
+                </tr>
+                <tr>
+                    <td>x</td>
+                    <td>x</td>
+                    <td>x</td>
+                    <td>x</td>
                 </tr>
                 <tr>
                     <td>Energia ACL:</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td>{energiaACL}</td>
+                    <td>{energiaACLTarifa}</td>
+                    <td>{totalEnergiaACL}</td>
                 </tr>
                 <tr>
                     <td>ICMS</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td> </td>
+                    <td>{livreIcms}</td>
+                    <td>{totalIcms}</td>
                 </tr>
                 <tr>
                     <td></td>
@@ -113,13 +127,19 @@ export default function Simulator(props){
                     <td>Total Livre:</td>
                     <td>xx</td>
                     <td>xx</td>
-                    <td></td>
+                    <td>{totalLivre}</td>
                 </tr>
                 <tr>
                     <td>Economia:</td>
                     <td>xx</td>
                     <td>xx</td>
-                    <td></td>
+                    <td>{economiaLivre}</td>
+                </tr>
+                <tr>
+                    <td>Porcetagem:</td>
+                    <td>xx</td>
+                    <td>xx</td>
+                    <td>{economiaPorcentagem}</td>
                 </tr>
             </table>
         </Table>
