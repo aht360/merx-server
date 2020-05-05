4cf21a0cbd97a6e5948722effd46a29e846e9ac6
Jhonatan <jhonatankennedy@live.com>
2020-05-05 15:47:20 -0300

    calcs + deploy

--- src/pages/Projetos/index.js
+++ src/pages/Projetos/index.js
@@ -1,8 +1,25 @@
-
 import React from 'react'
+import api from '../../Services/api'
 
 export default function MercadoLivre(){
+    async function handleClick(e){
+        e.preventDefault()
+    try {
+        const results = await api.get('elivelton');
+        console.log(results)
+      } catch (err) {
+        alert('Erro, tente novamente.');
+      }
+    }
+
     return(
-        <h1> Projetos work in progress</h1>
+        <div>
+           <h1> Projetos work in progress</h1> 
+            <form onSubmit={handleClick}>
+                <button type="submit"></button>
+            </form>
+
+        </div>
+        
     )
 }
