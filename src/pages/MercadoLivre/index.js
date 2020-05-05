d8fbcf578a6265690f9cd861ca7c531ab2e7352a
Jhonatan <jhonatankennedy@live.com>
2020-05-05 16:50:21 -0300

    procfile

--- src/pages/MercadoLivre/index.js
+++ src/pages/MercadoLivre/index.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import api from '../../Services/api'
 
 export default function MercadoLivre(){
+
+    async function handleSubmit(e){
+        e.preventDefault();
+    
+        try {
+            const results = await api.get('simulacao');
+
+          } catch (err) {
+            alert('Erro, tente novamente.');
+          }
+    }
+
     return(
-        <h1> Mercado Livre work in progress</h1>
+        <div>
+            <h1> Mercado Livre work in progress</h1>
+            <form onSubmit>
+                
+            </form>
+        </div>
+        
     )
 }
